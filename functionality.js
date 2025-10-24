91a21ab445191579a8495d1bf3e4a4afb8adbd05
envisionspacetesting <envisionspace.testing@gmail.com>
2025-10-24 14:37:41 +0530

    Update functionality.js

--- functionality.js
+++ functionality.js
@@ -22,14 +22,14 @@ const closeBtn = document.getElementById("closeModal");
 
 // Example default description (can be changed per project)
 const defaultDesc = `
-<p>With a built up area of over 45,000 sq ft, The Hinge is a mansion combining two of four villas commissioned to ‪@studioessteam‬. This has been designed for the family of Mr. Arjanbhai Dholakia of SRK Exports, one of the global leaders in Diamond industry. This project houses independent villas for his two sons - Mr. Ishver Dholakia and Mr. Akshay Dholakia, and is a part of a larger residential campus.</p>
+<p>With a built up area of over 45,000 sq ft, The Hinge is a mansion combining two of four villas commissioned to me‬. This has been designed for the family of Mr. Arjanbhai Dholakia of SRK Exports, one of the global leaders in Diamond industry. This project houses independent villas for his two sons - Mr. Ishver Dholakia and Mr. Akshay Dholakia, and is a part of a larger residential campus.</p>
 
 <p>The villa is thoughtfully planned to cater to the distinct needs of two families while presenting a unified architectural form. It is essentially two homes combined into one expansive structure, connected by a central rotunda that serves as a grand living space. Each of the two homes has eight bedrooms, living and family rooms, as well as elaborate kitchen and dining areas. The design ensures privacy for both families while promoting communal living through shared spaces.</p>
 
 <p>This interesting villa was designed and constructed in a record time of 15 months. It endeavours to capture the grandeur of an American Neo-Classical Villa as well as elegant and functional Modern living.</p>
 
-<p>Architects: ‪‪@studioessteam‬</p>
-<p>Video Credits: ‪‪@2feetdistance‬</p>
+<p>Architects: ‪‪</p>
+<p>Video Credits: ‪‪‬</p>
 `;
 
 document.querySelectorAll(".project-card").forEach(card => {
